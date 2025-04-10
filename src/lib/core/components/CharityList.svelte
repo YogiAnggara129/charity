d91a2afa822621a91c558463c897aba55803472f
Yogi Anggara <yogianggaraaa@gmail.com>
2025-04-10 07:10:07 +0700

    feat(home): slicing home page and its components (charity, footer, header, promo, welcome)

---
+++ src/lib/core/components/CharityList.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { CharityEntity } from "$lib/domain/charity.entity";
+	import CharityCard from "./CharityCard.svelte";
+
+	export let charities: CharityEntity[];
+</script>
+
+<section id="popularcause" class="bg-gray waypoint-tigger xs-section-padding">
+	<div class="container">
+		<div class="xs-heading row xs-mb-60">
+			<div class="col-md-9 col-xl-9">
+				<h2 class="xs-title">Popular Causes</h2>
+				<span class="xs-separetor dashed"></span>
+				<p>
+					FundPress has built a platform focused on aiding entrepreneurs,
+					startups, and <br /> companies raise capital from anyone.
+				</p>
+			</div>
+			<!-- .xs-heading-title END -->
+
+			{#if charities.length != 0}
+				{#each charities as charity}
+					<CharityCard {charity} />
+				{/each}
+			{/if}
+		</div>
+		<!-- .container end -->
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from "$lib/core/components/Header.svelte";
+	import CharityList from "$lib/core/components/CharityList.svelte";
+	import Welcome from "$lib/core/components/Welcome.svelte";
+	import Promo from "$lib/core/components/Promo.svelte";
+	import Footer from "$lib/core/components/Footer.svelte";
+	import { CharityEntity } from "$lib/domain/charity.entity";
+
+	let title = "Charity";
+	let charities = [
+		new CharityEntity(
+			1,
+			"First Charity Project",
+			"Money",
+			"https://buffer.com/resources/content/images/2024/11/free-stock-image-sites.png",
+			5000,
+			100000,
+			new Date("10 August 2020"),
+			"https://www.shutterstock.com/image-photo/happy-older-business-man-executive-260nw-2362307153.jpg",
+			"Viroide Bueno",
+			0
+		),
+		new CharityEntity(
+			2,
+			"Second Charity Project",
+			"Money",
+			"https://buffer.com/resources/content/images/2024/11/free-stock-image-sites.png",
+			5000,
+			100000,
+			new Date("10 August 2020"),
+			"https://www.shutterstock.com/image-photo/happy-older-business-man-executive-260nw-2362307153.jpg",
+			"Viroide Bueno",
+			0
+		),
+		new CharityEntity(
+			3,
+			"Third Charity Project",
+			"Money",
+			"https://buffer.com/resources/content/images/2024/11/free-stock-image-sites.png",
+			5000,
+			100000,
+			new Date("10 August 2020"),
+			"https://www.shutterstock.com/image-photo/happy-older-business-man-executive-260nw-2362307153.jpg",
+			"Viroide Bueno",
+			0
+		),
+		new CharityEntity(
+			4,
+			"Fourth Charity Project",
+			"Money",
+			"https://buffer.com/resources/content/images/2024/11/free-stock-image-sites.png",
+			5000,
+			100000,
+			new Date("10 August 2020"),
+			"https://www.shutterstock.com/image-photo/happy-older-business-man-executive-260nw-2362307153.jpg",
+			"Viroide Bueno",
+			0
+		),
+	];
+</script>
+
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
