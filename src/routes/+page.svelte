0d634a3b4f78b6ef2cf80769c58cbadc92e2ae4d
Yogi Anggara <yogianggaraaa@gmail.com>
2025-04-12 08:31:55 +0700

    build: change to adapter-static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 	import Promo from "$lib/core/components/Promo.svelte";
 	import Footer from "$lib/core/components/Footer.svelte";
 	import { CharityEntity } from "$lib/domain/charity.entity";
-
+	
 	let title = "Charity";
 	let charities = [
 		new CharityEntity(
